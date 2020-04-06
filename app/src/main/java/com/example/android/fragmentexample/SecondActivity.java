09a8c80489754f583c70cc37677d73dec710688b
Isaac Bevis <isaacbevis123@gmail.com>
2020-04-06 10:29:38 -0400

    Homework

--- app/src/main/java/com/example/android/fragmentexample/SecondActivity.java
+++ app/src/main/java/com/example/android/fragmentexample/SecondActivity.java
@@ -1,22 +1,5 @@
-/*
- * Copyright (C) 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.fragmentexample;
 
-import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -24,8 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class SecondActivity extends AppCompatActivity {
     private Button mButton;
     private boolean isFragmentDisplayed = false;
     static final String STATE_FRAGMENT = "state_of_fragment";
@@ -33,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
         mButton = findViewById(R.id.open_button);
 
@@ -96,8 +78,7 @@ public class MainActivity extends AppCompatActivity {
         isFragmentDisplayed = false;
     }
 
-    public void nextButton(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+    public void backButton(View view){
+        finish();
     }
 }
